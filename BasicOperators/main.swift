31bfa1f5c994881b8bdaaa8dda527ea3828cc5ae
liyang_a <771510512@qq.com>
2016-04-29 12:02:57 +0800

    1

---
+++ BasicOperators/main.swift
@@ -0,0 +1,60 @@
+//
+//  main.swift
+//  BasicOperators
+//
+//  Created by 李阳 on 16/4/29.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+///Nil Coalescing Operator
+let defaultColorName = "red"
+var userDefinedColorName: String?   // defaults to nil
+
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+// userDefinedColorName is nil, so colorNameToUse is set to the default of "red"
+
+userDefinedColorName = "green"
+colorNameToUse = userDefinedColorName ?? defaultColorName
+// userDefinedColorName is not nil, so colorNameToUse is set to "green"
+
+
+
+
+///Range Operators
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+// 1 times 5 is 5
+// 2 times 5 is 10
+// 3 times 5 is 15
+// 4 times 5 is 20
+// 5 times 5 is 25
+
+
+
+
+///Half-Open Range Operator
+let names = ["Anna", "Alex", "Brian", "Jack"]
+let count = names.count
+for i in 0..<count {
+    print("Person \(i + 1) is called \(names[i])")
+}
+// Person 1 is called Anna
+// Person 2 is called Alex
+// Person 3 is called Brian
+// Person 4 is called Jack
+
+
+
+
+
+
+
+
+
+
+
